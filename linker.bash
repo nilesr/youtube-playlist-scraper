3f8b0fed0654aea24c64962e2146e31e1d6b5435
Niles Rogoff <nilesrogoff@gmail.com>
2015-09-05 21:14:34 -0400

    Fixed critical bug that was causing links to not be made

--- linker.bash
+++ linker.bash
@@ -5,10 +5,10 @@ while read line; do
 	id=$(echo "$line"|awk '{print $3}')
 	test "$id" = "" && continue
 	mkdir "$playlist" 2>/dev/null && echo Created directory "$playlist"
-	currentfilename=$(find . -name '*'"$id"'*')
+	currentfilename=$(find . -name '*'"$id"'*' -maxdepth 1)
 	if test "$currentfilename" = ""; then
 	   	youtube-dl -x --audio-format mp3 -w -C -i "http://youtube.com/watch?v=$id"
-		currentfilename=$(find . -name '*'"$id"'*')
+		currentfilename=$(find . -name '*'"$id"'*' -maxdepth 1)
 	fi
 	newfilename="$idx. "$(echo "$currentfilename"|awk '{print substr($0, 3)}')
 	ln -s "$PWD/$currentfilename" "$playlist"/"$newfilename"
